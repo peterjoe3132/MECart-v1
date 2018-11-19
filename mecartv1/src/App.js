c9218880b07232d2ff7b78ca0f6e215913ee9365
Vaishnav-Ullas <vaishnavullas98@gmail.com>
2018-11-19 17:19:20 +0530

    modified:   mecartv1/package-lock.json 	modified:   mecartv1/package.json 	modified:   mecartv1/src/App.css 	modified:   mecartv1/src/App.js

--- mecartv1/src/App.js
+++ mecartv1/src/App.js
@@ -1,15 +1,63 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
-          
+        <header className="App-header">
+          <p>
+          <Link to="/buy"><button >BUY</button></Link>
+          <Link to="/sell"><button >SELL</button></Link>
+          </p>
+        </header>
+        <Route path="/buy" component={Buypage} />
+        <Route path="/sell" component={Sellpage} />
       </div>
+      </Router>
     );
   }
 }
 
+class Buypage extends Component {
+  render() {
+    return (<div className="Buy">
+    <header className="Buy-header">
+      <p>
+      BUY PAGE
+      </p>
+    </header>
+    <div class="grid-container">
+  <div class="grid-item">1</div>
+  <div class="grid-item">2</div>
+  <div class="grid-item">3</div>
+  <div class="grid-item">4</div>
+  <div class="grid-item">5</div>
+  <div class="grid-item">6</div>
+  <div class="grid-item">7</div>
+  <div class="grid-item">8</div>
+  <div class="grid-item">9</div>
+</div> 
+
+  </div>
+    );
+  }
+}
+
+class Sellpage extends Component {
+  render() {
+    return (<div className="Sell" >
+    <header className="sell-header">
+      <p>
+      SELL PAGE
+      </p>
+    </header>
+  </div>
+    );
+  }
+}
 export default App;
