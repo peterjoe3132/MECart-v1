e40cfdaf24212e0a61ce8d025a295bb12773c659
peterjoe3132 <peterjoe3132@gmail.com>
2018-11-19 23:24:49 +0530

    new file:   src/Components/EditAd/EditAd.css 	new file:   src/Components/EditAd/EditAd.js 	new file:   src/Components/NewAd/NewAd.css 	new file:   src/Components/NewAd/NewAd.js 	modified:   src/Components/SellPage/SellPage.css 	modified:   src/Components/SellPage/SellPage.js

---
+++ mecartv1/src/Components/EditAd/EditAd.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import './EditAd.css';
+
+class EditAd extends Component{
+    render(){
+        return(
+            <div className="edit">
+                   <h1>Hello</h1>
+            </div>
+         
+        );
+    }
+}
+
+export default EditAd
---
+++ mecartv1/src/Components/NewAd/NewAd.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import './NewAd.css';
+
+class NewAd extends Component{
+    render(){
+        return(
+            <div className="new">
+                   <h1>Hi</h1>
+            </div>
+         
+        );
+    }
+}
+
+export default NewAd
