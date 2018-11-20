b953b9227dc3d7df49bd7d1d06559586d65f593a
Suby <subincbenny7@gmail.com>
2018-11-21 00:17:43 +0530

    Changes to be committed: 	modified:   src/Components/SellPage/SellPage.css 	modified:   src/Components/SellPage/SellPage.js

--- mecartv1/src/Components/SellPage/SellPage.js
+++ mecartv1/src/Components/SellPage/SellPage.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import './SellPage.css';
 
-
+import { Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
 import EditAd from '../EditAd/EditAd'
 import NewAd from '../NewAd/NewAd'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
@@ -24,15 +24,40 @@ class SellPage extends Component {
         //   </div>
 
         // </Router>
-        
+
+
+
+
+
         <Router>
       <div className="Sell">
-        <header className="Sellbuttons">
-          <p>
-          <Link to="/edit"><button className="primary" >EDIT MY AD</button></Link>
-          <Link to="/newAd"><button >NEW AD</button></Link>
-          </p>
-        </header>
+          <Row >
+              <Col sm="6">
+                <Card body inverse style={{ backgroundColor: '#333', borderColor: 'rgba(0,0,0,0)', padding:'18%', height:'60rem'}}>
+
+                    <CardTitle style={{fontSize:'3rem', padding:'20px'}}>Edit My Add</CardTitle>
+                    <CardText style={{ textAlign:'justify', textJustify:'inter-word', padding:'40px'}}>
+                    It is a long established fact that a reader will be distracted by the readable 
+                    content of a page when looking at its layout. The point of using Lorem Ipsum is that it 
+                    has a more-or-less normal distribution of letters, as opposed to using 'Content here, 
+                    content here', making it look like readable English.
+                </CardText>
+                         <Link to="/edit"><Button color="primary" size="lg" block>Edit</Button></Link>
+                </Card>
+              </Col>
+             <Col sm="6">
+                <Card body style={{borderColor: 'white', padding:'18%', height:'100%',width:'100%',border:'none'}}>
+                    <CardTitle style={{fontSize:'3rem', padding:'20px'}}>New Add</CardTitle>
+                    <CardText style={{ textAlign:'justify', textJustify:'inter-word', padding:'40px'}}>
+                    It is a long established fact that a reader will be distracted by the readable 
+                    content of a page when looking at its layout. The point of using Lorem Ipsum is that it 
+                    has a more-or-less normal distribution of letters, as opposed to using 'Content here, 
+                    content here', making it look like readable English.
+                </CardText>
+                         <Link to="/newAd"><Button color="primary" size="lg" block>New</Button></Link>
+                </Card>
+              </Col>
+              </Row>
         <Route path="/edit" component={EditAd} />
         <Route path="/newAd" component={NewAd} />
       </div>
