aa86a65b6b6db203b64a01b42615c000f91d09b4
Suby <subincbenny7@gmail.com>
2018-11-20 13:55:43 +0530

    Changes to be committed: 	modified:   src/Components/EditAd/EditAd.js 	modified:   src/Components/NewAd/NewAd.js

--- mecartv1/src/Components/NewAd/NewAd.js
+++ mecartv1/src/Components/NewAd/NewAd.js
@@ -5,9 +5,9 @@ import './NewAd.css';
 class NewAd extends Component{
     render(){
         return(
-            
+            <div className="new">
                    <h1>Hi</h1>
-            
+            </div>
          
         );
     }
