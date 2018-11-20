69599920324d5988ac638311beeb76c0908f01f7
Vaishnav-Ullas <vaishnavullas98@gmail.com>
2018-11-20 10:54:50 +0530

    modified:   mecartv1/src/Components/BuyPage/BuyPage.js 	new file:   mecartv1/src/Components/Product/Product.css 	new file:   mecartv1/src/Components/Product/Product.js

---
+++ mecartv1/src/Components/Product/Product.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Product.css';
+class Product extends Component {
+    render() {
+      return (<div className="product">
+      <header className="Buy-header">
+        <p>
+       Product Page
+        </p>
+      </header>  
+    </div>
+      );
+    }
+  }
+
+  export default Product
