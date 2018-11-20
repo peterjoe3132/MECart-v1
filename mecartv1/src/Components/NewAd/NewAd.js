f56528f8052250c7b92362834ee4ac0bd296cca7
Suby <subincbenny7@gmail.com>
2018-11-20 14:57:25 +0530

    Changes to be committed: 	modified:   src/Components/NewAd/NewAd.css 	modified:   src/Components/NewAd/NewAd.js

--- mecartv1/src/Components/NewAd/NewAd.js
+++ mecartv1/src/Components/NewAd/NewAd.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import { Col, Row, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 import './NewAd.css';
 
 class NewAd extends Component{
@@ -7,8 +7,57 @@ class NewAd extends Component{
         return(
             <div className="new">
                    <h1>Hi</h1>
-            </div>
-         
+             <Form>
+        <Row form>
+          <Col md={6}>
+            <FormGroup>
+              <Label for="exampleEmail">Email</Label>
+              <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+            </FormGroup>
+          </Col>
+          <Col md={6}>
+            <FormGroup>
+              <Label for="examplePassword">Password</Label>
+              <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
+            </FormGroup>
+          </Col>
+        </Row>
+        <FormGroup>
+          <Label for="exampleAddress">Address</Label>
+          <Input type="text" name="address" id="exampleAddress" placeholder="1234 Main St"/>
+        </FormGroup>
+        <FormGroup>
+          <Label for="exampleAddress2">Address 2</Label>
+          <Input type="text" name="address2" id="exampleAddress2" placeholder="Apartment, studio, or floor"/>
+        </FormGroup>
+        <Row form>
+          <Col md={6}>
+            <FormGroup>
+              <Label for="exampleCity">City</Label>
+              <Input type="text" name="city" id="exampleCity"/>
+            </FormGroup>
+          </Col>
+          <Col md={4}>
+            <FormGroup>
+              <Label for="exampleState">State</Label>
+              <Input type="text" name="state" id="exampleState"/>
+            </FormGroup>
+          </Col>
+          <Col md={2}>
+            <FormGroup>
+              <Label for="exampleZip">Zip</Label>
+              <Input type="text" name="zip" id="exampleZip"/>
+            </FormGroup>  
+          </Col>
+        </Row>
+        <FormGroup check>
+          <Input type="checkbox" name="check" id="exampleCheck"/>
+          <Label for="exampleCheck" check>Check me out</Label>
+        </FormGroup>
+        <Button>Sign in</Button>
+      </Form>
+
+         </div>
         );
     }
 }
