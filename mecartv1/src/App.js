e5b9a7eeadc4a8dbc6b7c4d8ad7c15ace46669ed
Suby <subincbenny7@gmail.com>
2018-11-20 15:29:32 +0530

    Changes to be committed: 	modified:   src/App.css 	modified:   src/App.js

--- mecartv1/src/App.js
+++ mecartv1/src/App.js
@@ -12,26 +12,26 @@ class App extends Component {
 	    	<div id="body">
 	    	   <Row>
 	      			<Col sm="6">
-	       				<Card  body inverse style={{ backgroundColor: 'rgba(0,0,0,0)', borderColor: 'rgba(0,0,0,0)', padding:'18%'}}>
+	       				<Card body inverse style={{ backgroundColor: '#333', borderColor: 'rgba(0,0,0,0)', padding:'18%', height:'100vh'}}>
 	          				<CardTitle style={{fontSize:'3rem', padding:'20px'}}>Buy</CardTitle>
 	          				<CardText style={{ textAlign:'justify', textJustify:'inter-word', padding:'40px'}}>
 	          				It is a long established fact that a reader will be distracted by the readable 
 	          				content of a page when looking at its layout. The point of using Lorem Ipsum is that it 
 	          				has a more-or-less normal distribution of letters, as opposed to using 'Content here, 
 	          				content here', making it look like readable English.
-	          				</CardText>
+	          				</CardText><br/><br/><br/>
 	           					<Link to="/buy"><Button color="primary" size="lg" block>Buy</Button></Link>
 	        			</Card>
 	      			</Col>
 	      			<Col sm="6">
-	        			<Card body style={{borderColor: 'white', padding:'18%' }}>
+	        			<Card body style={{borderColor: 'white', padding:'18%', height:'100vh'}}>
 	          				<CardTitle style={{fontSize:'3rem', padding:'20px'}}>Sell</CardTitle>
 	          				<CardText style={{ textAlign:'justify', textJustify:'inter-word', padding:'40px'}}>
 	          				It is a long established fact that a reader will be distracted by the readable 
 	          				content of a page when looking at its layout. The point of using Lorem Ipsum is that it 
 	          				has a more-or-less normal distribution of letters, as opposed to using 'Content here, 
 	          				content here', making it look like readable English.
-	          				</CardText>
+	          				</CardText><br/><br/><br/>
 	          					<Link to="/sell"><Button color="primary" size="lg" block>Sell</Button></Link>
 	        			</Card>
 	      			</Col>
