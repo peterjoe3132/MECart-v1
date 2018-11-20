8ccc17319d535fc66b853194c7e462c1e8296e77
Suby <subincbenny7@gmail.com>
2018-11-20 12:25:07 +0530

    Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.js 	modified:   src/index.js

--- mecartv1/src/App.js
+++ mecartv1/src/App.js
@@ -1,35 +1,37 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-
-
+import { Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
 import './App.css';
-
-
-
 import  BuyPage from "./Components/BuyPage/BuyPage.js";
 import SellPage from "./Components/SellPage/SellPage.js"
-
 
 class App extends Component {
   render() {
     return (
-      <Router>
-      <div className="App">
-        <header className="App-header">
-          <p>
-          <Link to="/buy"><button >BUY</button></Link>
-          <Link to="/sell"><button >SELL</button></Link>
-          </p>
-        </header>
-        <Route path="/buy" component={BuyPage} />
-        <Route path="/sell" component={SellPage} />
-      </div>
-      </Router>
+    	<Router>
+	    	<div>
+	    	   <Row>
+	      			<Col sm="6">
+	       				<Card body>
+	          				<CardTitle>Special Title Treatment</CardTitle>
+	          				<CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+	           					<Link to="/buy"><Button>Go somewhere</Button></Link>
+	        			</Card>
+	      			</Col>
+	      			<Col sm="6">
+	        			<Card body>
+	          				<CardTitle>Special Title Treatment</CardTitle>
+	          				<CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+	          					<Link to="/sell"><Button>Go somewhere</Button></Link>
+	        			</Card>
+	      			</Col>
+	    		</Row>
+	    	<Route path="/buy" component={BuyPage} />
+	    	<Route path="/sell" component={SellPage} />   
+	    	</div>
+        </Router>
     );
   }
 }
-
-
-
 
 export default App;
