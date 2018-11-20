a860b7128c1c53f9e004ee636be9e57236fad804
peterjoe3132 <peterjoe3132@gmail.com>
2018-11-20 23:21:08 +0530

    modified:   src/App.css 	modified:   src/App.js 	modified:   src/Components/BuyPage/BuyPage.css 	modified:   src/Components/NewAd/NewAd.css 	modified:   src/Components/NewAd/NewAd.js 	new file:   src/Components/NewAd/background1.jpg 	new file:   src/Components/NewAd/bootstrap.css 	new file:   src/Components/NewAd/logo.png

--- mecartv1/src/App.js
+++ mecartv1/src/App.js
@@ -12,27 +12,30 @@ class App extends Component {
 	    	<div id="body">
 	    	   <Row>
 	      			<Col sm="6">
-	       				<Card body inverse style={{ backgroundColor: '#333', borderColor: 'rgba(0,0,0,0)', padding:'18%', height:'100vh'}}>
+	       				<Card body inverse style={{ backgroundColor: '#333', borderColor: 'rgba(0,0,0,0)', padding:'8%', height:'100%' ,width:'100%',border:'0%'}}>
 	          				<CardTitle style={{fontSize:'3rem', padding:'20px'}}>Buy</CardTitle>
 	          				<CardText style={{ textAlign:'justify', textJustify:'inter-word', padding:'40px'}}>
 	          				It is a long established fact that a reader will be distracted by the readable 
 	          				content of a page when looking at its layout. The point of using Lorem Ipsum is that it 
 	          				has a more-or-less normal distribution of letters, as opposed to using 'Content here, 
 	          				content here', making it look like readable English.
-	          				</CardText>
-	           					<Link to="/buy"><Button color="primary" size="lg" block>Buy</Button></Link>
+	          				<Link to="/buy"><Button color="primary" size="lg" block>Buy</Button></Link>
+							  </CardText>
+	           					{/* <Link to="/buy"><Button color="primary" size="lg" block>Buy</Button></Link> */}
 	        			</Card>
 	      			</Col>
 	      			<Col sm="6">
-	        			<Card body style={{borderColor: 'white', padding:'18%', height:'100vh'}}>
+	        			<Card body style={{borderColor: 'white', padding:'8%', height:'100%',width:'100%',border:'none'}}>
 	          				<CardTitle style={{fontSize:'3rem', padding:'20px'}}>Sell</CardTitle>
 	          				<CardText style={{ textAlign:'justify', textJustify:'inter-word', padding:'40px'}}>
 	          				It is a long established fact that a reader will be distracted by the readable 
 	          				content of a page when looking at its layout. The point of using Lorem Ipsum is that it 
 	          				has a more-or-less normal distribution of letters, as opposed to using 'Content here, 
 	          				content here', making it look like readable English.
-	          				</CardText>
-	          					<Link to="/sell"><Button color="primary" size="lg" block>Sell</Button></Link>
+	          				
+							  <Link to="/sell"><Button color="primary" size="lg" block>Sell</Button></Link>
+							  </CardText>
+	          					
 	        			</Card>
 	      			</Col>
 	    		</Row>
--- mecartv1/src/Components/NewAd/NewAd.js
+++ mecartv1/src/Components/NewAd/NewAd.js
@@ -1,70 +1,192 @@
-import React, { Component } from 'react';
-import { Col, Row, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
-import './NewAd.css';
+// import React, { Component } from 'react';
+// import { Col, Row, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+// import './NewAd.css';
+// import './bootstrap.css'
+// class NewAd extends Component{
+//     render(){
+//         return(
+//             <div className="new">
+//             <Form >
+//         <Row form >
+//           <Col md={6}>
+//             <FormGroup inline >
+//               <Label for="exampleEmail" className="mr-sm-2">Email</Label>
+//               <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+//             </FormGroup>
+//           </Col>
+//           <Col md={6}>
+//             <FormGroup>
+//               <Label for="examplePassword">Password</Label>
+//               <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
+//             </FormGroup>
+//           </Col>
+//         </Row>
+//         <FormGroup>
+//           <Label for="exampleAddress">Address</Label>
+//           <Input type="text" name="address" id="exampleAddress" placeholder="1234 Main St"/>
+//         </FormGroup>
+//         <FormGroup>
+//           <Label for="exampleAddress2">Address 2</Label>
+//           <Input type="text" name="address2" id="exampleAddress2" placeholder="Apartment, studio, or floor"/>
+//         </FormGroup>
+//         <Row form>
+//           <Col md={6}>
+//             <FormGroup>
+//               <Label for="exampleCity">City</Label>
+//               <Input type="text" name="city" id="exampleCity"/>
+//             </FormGroup>
+//           </Col>
+//           <Col md={4}>
+//             <FormGroup>
+//               <Label for="exampleState">State</Label>
+//               <Input type="text" name="state" id="exampleState"/>
+//             </FormGroup>
+//           </Col>
+//           <Col md={2}>
+//             <FormGroup>
+//               <Label for="exampleZip">Zip</Label>
+//               <Input type="text" name="zip" id="exampleZip"/>
+//             </FormGroup>  
+//           </Col>
+//         </Row>
+//         <FormGroup check>
+//           <Input type="checkbox" name="check" id="exampleCheck"/>
+//           <Label for="exampleCheck" check>Check me out</Label>
+//         </FormGroup>
+//         <Button>Sign in</Button>
+//       </Form>
+
+//          </div>
+//         );
+//     }
+// }
+
+// export default NewAd
+
+
+
+
+
+import React,{Component} from 'react'
+import './NewAd.css'
+import logo from './logo.png'
+import './bootstrap.css'
 
 class NewAd extends Component{
     render(){
         return(
             <div className="new">
-             <Form>
-        <Row form>
-          <Col md={6}>
-            <FormGroup>
-              <Label for="exampleEmail">Product Name</Label>
-              <Input type="email" name="email" id="exampleEmail" placeholder="" />
-            </FormGroup>
-          </Col>
-          <Col md={6}>
-            <FormGroup>
-          <Label for="exampleSelect">Select</Label>
-          <Input type="select" name="select" id="exampleSelect">
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
-          </Input>
-        </FormGroup>
-          </Col>
-        </Row>
-        
-         <FormGroup>
-          <Label for="exampleText">Product Description</Label>
-          <Input type="textarea" name="text" id="exampleText" />
-        </FormGroup>
-        <FormGroup>
-          <Label for="exampleFile">Photo</Label>
-          <Input type="file" name="file" id="exampleFile" />
-          <FormText color="muted">
-            Upload Photo
-          </FormText>
-        </FormGroup>
-        <Row form>
-          <Col md={6}>
-            <FormGroup>
-              <Label for="exampleCity">Name</Label>
-              <Input type="text" name="city" id="exampleCity"/>
-            </FormGroup>
-          </Col>
-          <Col md={4}>
-            <FormGroup>
-          <Label for="exampleNumber">Number</Label>
-          <Input type="number" name="number" id="exampleNumber" placeholder="number placeholder" />
-        </FormGroup>
-          </Col>
-          <Col md={2}>
-             <FormGroup>
-              <Label for="exampleCity">City</Label>
-              <Input type="text" name="city" id="exampleCity"/>
-            </FormGroup>
-          </Col>
-        </Row>
-       
-      </Form>
+            <div className="formstyle">
 
-         </div>
+             {/* <img className="logo" src={logo} width="100" height="100"/> */}
+            <form className="unit">
+                    <div className="form-group ">
+                        <label for="AdTitle">Ad Title *</label>
+                        <input type="text" className="form-control" id="AdTitle" placeholder="Enter Title here" required maxLength="25"/>
+                        <small id="AdTitleHelpBlock" class="form-text text-muted">
+                        Title should not exceed 25 characters
+                        </small>                 
+                    </div>
+                 <div className="row">
+                            <div className="form-group col" >
+                                <label for="category" required>Category*</label>
+                                <select className="form-control" id="Category" required >
+                                <option value="0">Select Category:</option>
+                                <option>Stationary</option>
+                                <option>Books</option>
+                                <option>Electronics</option>
+                                {/* <option></option> */}
+                                </select>
+                            </div>
+
+                            <div className="form-group col">
+                                <label for="AdDescription">Ad Description*</label>
+                                <textarea className="form-control" id="AdDescription" rows="3" required maxLength="200"></textarea>
+                                <small id="passwordHelpBlock" class="form-text text-muted">
+                                Should not exceed 200 words
+                                </small>
+                    </div>
+                    
+                    </div>
+                   
+                    
+                   <div className="row">
+
+                        <div className="form-group col">
+                            <label for="AdPhotos">Upload Photos*</label>
+                            <input type="text" className="form-control" id="AdPhotos" placeholder="Insert URL" required/>
+                            <small id="passwordHelpBlock" class="form-text text-muted">
+                            Tip:Photos attract buyers
+                            </small>
+                        </div>
+
+
+                        <div className="form-group col">
+                            <label for="AdPrice">Price *</label>
+                            <input type="number" className="form-control" id="AdPrice" placeholder="Enter Price here" required />
+                                        
+                        </div>
+                    
+                   </div>
+                   <hr></hr>
+                  
+
+                   <div className="row">
+
+                    <div className="form-group col ">
+                        <label for="ContactName">Name *</label>
+                        <input type="text" className="form-control" id="ContactName" placeholder="Enter Name here" required maxLength="25"/>
+                        <small id="AdTitleHelpBlock" className="form-text text-muted">
+                            Name to be displayed on the Ad
+                        </small>                 
+                    </div>
+
+                     <div className="form-group col ">
+                        <label for="ContactNum">Phone *</label>
+                        <input type="tel" className="form-control" id="ContactNum" placeholder="Enter Phone number here" required maxLength="10"/>
+                        <small id="AdTitleHelpBlock" className="form-text text-muted">
+                            Contact to be shown
+                        </small>                 
+                    </div>
+                   
+                   </div>
+                    
+                   
+                  <div className="row">
+                        {/* <div className="form-group col"  >
+                                <label for="category" required>Class*</label>
+                                <select className="form-control" id="Category" required >
+                                <option value="0">Select Class:</option>
+                                <option>Class 1</option>
+                                <option>Class 2</option>
+                                <option>Class 3</option>
+                                <option>Class 4</option>
+                                <option>Class 5</option>
+                                <option>Class 6</option>
+                                <option>Class 7</option>
+                                <option>Class 8</option>
+                                </select>
+                            </div>
+                             */}
+                        
+                            
+                            <div class="form-check col terms">
+                                <input class="form-check-input" type="checkbox" value="" id="Terms" required/>
+                                <label class="form-check-label" for="Terms">
+                                * I agree to the terms and conditions 
+                                </label>
+                            </div>
+                            <button className="btn btn-primary" type="submit">Post Ad</button>
+
+                   </div>
+                </form>
+              
+
+            </div>
+              
+
+            </div>
         );
     }
 }
-
 export default NewAd
