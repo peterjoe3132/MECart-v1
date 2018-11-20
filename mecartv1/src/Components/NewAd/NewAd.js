e596ae46f73338358a36eb595c149b537061bb7e
Joann <joannpelza0@gmail.com>
2018-11-20 15:50:08 +0530

    modified:   src/Components/NewAd/NewAd.js

--- mecartv1/src/Components/NewAd/NewAd.js
+++ mecartv1/src/Components/NewAd/NewAd.js
@@ -10,50 +10,56 @@ class NewAd extends Component{
         <Row form>
           <Col md={6}>
             <FormGroup>
-              <Label for="exampleEmail">Email</Label>
-              <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+              <Label for="exampleEmail">Product Name</Label>
+              <Input type="email" name="email" id="exampleEmail" placeholder="" />
             </FormGroup>
           </Col>
           <Col md={6}>
             <FormGroup>
-              <Label for="examplePassword">Password</Label>
-              <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
-            </FormGroup>
+          <Label for="exampleSelect">Select</Label>
+          <Input type="select" name="select" id="exampleSelect">
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+          </Input>
+        </FormGroup>
           </Col>
         </Row>
-        <FormGroup>
-          <Label for="exampleAddress">Address</Label>
-          <Input type="text" name="address" id="exampleAddress" placeholder="1234 Main St"/>
+        
+         <FormGroup>
+          <Label for="exampleText">Product Description</Label>
+          <Input type="textarea" name="text" id="exampleText" />
         </FormGroup>
         <FormGroup>
-          <Label for="exampleAddress2">Address 2</Label>
-          <Input type="text" name="address2" id="exampleAddress2" placeholder="Apartment, studio, or floor"/>
+          <Label for="exampleFile">Photo</Label>
+          <Input type="file" name="file" id="exampleFile" />
+          <FormText color="muted">
+            Upload Photo
+          </FormText>
         </FormGroup>
         <Row form>
           <Col md={6}>
             <FormGroup>
-              <Label for="exampleCity">City</Label>
+              <Label for="exampleCity">Name</Label>
               <Input type="text" name="city" id="exampleCity"/>
             </FormGroup>
           </Col>
           <Col md={4}>
             <FormGroup>
-              <Label for="exampleState">State</Label>
-              <Input type="text" name="state" id="exampleState"/>
-            </FormGroup>
+          <Label for="exampleNumber">Number</Label>
+          <Input type="number" name="number" id="exampleNumber" placeholder="number placeholder" />
+        </FormGroup>
           </Col>
           <Col md={2}>
-            <FormGroup>
-              <Label for="exampleZip">Zip</Label>
-              <Input type="text" name="zip" id="exampleZip"/>
-            </FormGroup>  
+             <FormGroup>
+              <Label for="exampleCity">City</Label>
+              <Input type="text" name="city" id="exampleCity"/>
+            </FormGroup>
           </Col>
         </Row>
-        <FormGroup check>
-          <Input type="checkbox" name="check" id="exampleCheck"/>
-          <Label for="exampleCheck" check>Check me out</Label>
-        </FormGroup>
-        <Button>Sign in</Button>
+       
       </Form>
 
          </div>
