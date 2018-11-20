b2eb6b63b0d82f200bff2b40b0385a1f42ef6e4d
Suby <subincbenny7@gmail.com>
2018-11-20 23:52:04 +0530

    Changes to be committed: 	modified:   src/App.js 	modified:   src/Components/NewAd/NewAd.js

--- mecartv1/src/App.js
+++ mecartv1/src/App.js
@@ -10,36 +10,30 @@ class App extends Component {
     return (
     	<Router>
 	    	<div id="body">
-	    	   <Row sm="">
+	    	   <Row >
 	      			<Col sm="6">
-<<<<<<< HEAD
-	       				<Card body inverse style={{ backgroundColor: '#333', borderColor: 'rgba(0,0,0,0)', padding:'8%', height:'100%' ,width:'100%',border:'0%'}}>
-=======
 	       				<Card body inverse style={{ backgroundColor: '#333', borderColor: 'rgba(0,0,0,0)', padding:'18%', height:'60rem'}}>
->>>>>>> f2369457b836c15be06f4ad16f575cd0f1bc050f
+
 	          				<CardTitle style={{fontSize:'3rem', padding:'20px'}}>Buy</CardTitle>
 	          				<CardText style={{ textAlign:'justify', textJustify:'inter-word', padding:'40px'}}>
 	          				It is a long established fact that a reader will be distracted by the readable 
 	          				content of a page when looking at its layout. The point of using Lorem Ipsum is that it 
 	          				has a more-or-less normal distribution of letters, as opposed to using 'Content here, 
 	          				content here', making it look like readable English.
-	          				<Link to="/buy"><Button color="primary" size="lg" block>Buy</Button></Link>
 							  </CardText>
-	           					{/* <Link to="/buy"><Button color="primary" size="lg" block>Buy</Button></Link> */}
+	           					<Link to="/buy"><Button color="primary" size="lg" block>Buy</Button></Link>
 	        			</Card>
 	      			</Col>
 	      			<Col sm="6">
-	        			<Card body style={{borderColor: 'white', padding:'8%', height:'100%',width:'100%',border:'none'}}>
+	        			<Card body style={{borderColor: 'white', padding:'18%', height:'100%',width:'100%',border:'none'}}>
 	          				<CardTitle style={{fontSize:'3rem', padding:'20px'}}>Sell</CardTitle>
 	          				<CardText style={{ textAlign:'justify', textJustify:'inter-word', padding:'40px'}}>
 	          				It is a long established fact that a reader will be distracted by the readable 
 	          				content of a page when looking at its layout. The point of using Lorem Ipsum is that it 
 	          				has a more-or-less normal distribution of letters, as opposed to using 'Content here, 
 	          				content here', making it look like readable English.
-	          				
-							  <Link to="/sell"><Button color="primary" size="lg" block>Sell</Button></Link>
 							  </CardText>
-	          					
+	          					 <Link to="/sell"><Button color="primary" size="lg" block>Sell</Button></Link>
 	        			</Card>
 	      			</Col>
 	    		</Row>
