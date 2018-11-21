a8bcf6f68e9ef165834b1652a7df9ed468015fc2
Suby <subincbenny7@gmail.com>
2018-11-21 14:23:50 +0530

    Changes to be committed: 	modified:   src/Components/NewAd/NewAd.css 	modified:   src/Components/NewAd/NewAd.js

--- mecartv1/src/Components/NewAd/NewAd.js
+++ mecartv1/src/Components/NewAd/NewAd.js
@@ -1,8 +1,3 @@
-
-
-
-
-
 import React,{Component} from 'react'
 import './NewAd.css'
 import logo from './logo.png'
@@ -59,31 +54,31 @@ class NewAd extends Component{
              {/* <img className="logo" src={logo} width="100" height="100"/> */}
             <form className="unit" onSubmit={this.handlesubmit} >
                     <div className="form-group ">
-                        <label for="AdTitle">Ad Title *</label>
-                        <input type="text" className="form-control" name="AdTitle" id="AdTitle" placeholder="Enter Title here" required maxLength="25"/>
+                        <label for="AdTitle">Ad Title *</label><br/>
+                        <input type="text" className="text" name="AdTitle" id="AdTitle" placeholder="Enter Title here" required maxLength="25"/>
                         <small id="AdTitleHelpBlock" class="form-text text-muted">
                         Title should not exceed 25 characters
                         </small>                 
                     </div>
                  <div className="row">
                             <div className="form-group col" >
-                                <label for="category" required>Category*</label>
-                                <select className="form-control" id="Category" name="Category" required >
-                                <option value="0">Select Category:</option>
-                                <option>Stationary</option>
-                                <option>Books</option>
-                                <option>Electronics</option>
+                                <label for="category" required>Category*</label><br/>
+                                <select className="text" id="Category" name="Category" required >
+                                <option value="0" >Select Category:</option>
+                                <option style={{color:'black'}}>Stationary</option>
+                                <option style={{color:'black'}}>Books</option>
+                                <option style={{color:'black'}}>Electronics</option>
                                 {/* <option></option> */}
                                 </select>
                             </div>
 </div>
                          <div className="form-group col">
-                                <label for="AdDescription">Ad Description*</label>
-                                <textarea className="form-control" id="AdDescription" rows="3" name="AdDescription" required maxLength="200"></textarea>
-                                <small id="passwordHelpBlock" class="form-text text-muted">
+                                <label for="AdDescription">Ad Description*</label><br/>
+                                <textarea className="text" id="AdDescription" rows="3" name="AdDescription" required maxLength="200"></textarea>
+                                <small id="passwordHelpBlock" classNameass="form-text text-muted">
                                 Should not exceed 200 words
                                 </small>
-                                </div> }
+                                </div> 
                     
                
                    
@@ -91,8 +86,8 @@ class NewAd extends Component{
                    <div className="row">
 
                         <div className="form-group col">
-                            <label for="AdPhotos">Upload Photos*</label>
-                            <input type="text" className="form-control" id="AdPhotos" name="AdPhotos" placeholder="Insert URL" required/>
+                            <label for="AdPhotos">Upload Photos*</label><br/>
+                            <input type="text" className="text" id="AdPhotos" name="AdPhotos" placeholder="Insert URL" required/>
                             <small id="passwordHelpBlock" class="form-text text-muted">
                             Tip:Photos attract buyers
                             </small>
@@ -100,8 +95,8 @@ class NewAd extends Component{
 
 
                         <div className="form-group col">
-                            <label for="AdPrice">Price *</label>
-                            <input type="number" className="form-control" id="AdPrice" name="AdPrice"placeholder="Enter Price here" required />
+                            <label for="AdPrice">Price *</label><br/>
+                            <input type="number" className="text" id="AdPrice" name="AdPrice"placeholder="Enter Price here" required />
                                         
                         </div>
                     
@@ -112,16 +107,16 @@ class NewAd extends Component{
                    <div className="row">
 
                     <div className="form-group col ">
-                        <label for="ContactName">Name *</label>
-                        <input type="text" className="form-control" id="ContactName" name="ContactName" placeholder="Enter Name here" required maxLength="25"/>
+                        <label for="ContactName">Name *</label><br/>
+                        <input type="text" className="text" id="ContactName" name="ContactName" placeholder="Enter Name here" required maxLength="25"/>
                         <small id="AdTitleHelpBlock" className="form-text text-muted">
                             Name to be displayed on the Ad
                         </small>                 
                     </div>
 
                      <div className="form-group col ">
-                        <label for="ContactNum">Phone *</label>
-                        <input type="tel" className="form-control" id="ContactNum" name="ContactNum" placeholder="Enter Phone number here" required maxLength="10"/>
+                        <label for="ContactNum">Phone *</label><br/>
+                        <input type="tel" className="text" id="ContactNum" name="ContactNum" placeholder="Enter Phone number here" required maxLength="10"/>
                         <small id="AdTitleHelpBlock" className="form-text text-muted">
                             Contact to be shown
                         </small>                 
@@ -140,12 +135,13 @@ class NewAd extends Component{
                                 * I agree to the terms and conditions 
                                 </label>
                             </div>
-                            <button className="btn btn-primary" type="submit">Post Ad</button>
+                           
 
                    </div>
-                </form>
-              
 
+                </form>
+                    <br/><br/><br/>
+                 <button className="buttonP" type="submit">Post Ad</button>
             </div>
               
 
