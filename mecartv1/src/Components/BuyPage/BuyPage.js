31aa28290bb79d8edbca9753ac88af37acc49b03
peterjoe3132 <peterjoe3132@gmail.com>
2018-11-24 23:45:10 +0530

    modified:   src/Components/BuyPage/BuyPage.js intended & documented 	deleted:    src/Components/BuyPage/tst.js

--- mecartv1/src/Components/BuyPage/BuyPage.js
+++ mecartv1/src/Components/BuyPage/BuyPage.js
@@ -2,17 +2,19 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './BuyPage.css';
 import  Product from "../Product/Product";
-import GuttersGrid from './tst'
+
 
 
 class BuyPage extends Component {
   constructor(props) {
     super(props);
-
+    //states declared to read the different products
     this.state = { data: [] };
   }
-  componentDidMount() {
   
+  componentDidMount() {
+  //function to get data from the server .
+  //This function is called after the corresponding component is rendered correctly.
     var requestOptions = {
       "method": "POST",
       "headers": {
@@ -22,15 +24,19 @@ class BuyPage extends Component {
         'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With, Accept'
       }
     };
-    console.log(requestOptions);
+    // console.log(requestOptions);
+    //fetch request to the server
     fetch('http://127.0.0.1:8080/buyproduct',requestOptions)
     .then(res => res.json())
+    //json is used so as to get jus the body data
+    //the details of the product is stored in the data state in json format
     .then(json => this.setState({ data: json }));
     }
     render() {
       return (<div className="Buy">
       <header className="Buy-header">
       </header> 
+      {/* the map function is called to display all the items in the database using a loop */}
       {this.state.data.map(el => (
              <Item itemdetails={el}/>
           ))}  
@@ -42,8 +48,9 @@ class BuyPage extends Component {
   class Item extends Component {
     constructor(props){
       super(props);
+      //props decalred so as to read the different attributes of each product
       this.productname=this.props.itemdetails.product_name;
-      console.log(props);
+      // console.log(props); 
       this.imgsrc=this.props.itemdetails.imgurl;
       this.price=this.props.itemdetails.price;
       this.contname=this.props.itemdetails.contact_name
@@ -51,9 +58,10 @@ class BuyPage extends Component {
       }
     render() {
       return (
-        <Router>
+        //Each product is displayed in the following format
+        //the component Item is called with the details send as props
           <div style={{padding:'20px'}}>
-          <Link to="/product">
+          
             <div className="flex">
               <div> <img width="200px"  height="200px "src={this.imgsrc}/></div>
               <br/>
@@ -62,11 +70,10 @@ class BuyPage extends Component {
               <div>Contact Number:{this.phone}</div>
               <b>Price:{this.price}</b>
             </div>
-          </Link>    
-       <Route path="/product" component={Product} />
+              
+       
 
        </div>
-      </Router>
       );
     }
   }
