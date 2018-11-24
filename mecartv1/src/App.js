19db03c091b1dbd4aa161036a5466dee5c43e955
peterjoe3132 <peterjoe3132@gmail.com>
2018-11-24 23:00:26 +0530

    modified:   src/App.js intended

--- mecartv1/src/App.js
+++ mecartv1/src/App.js
@@ -8,6 +8,7 @@ import SellPage from "./Components/SellPage/SellPage.js"
 class App extends Component {
   render() {
     return (
+		// Router tag opened to route to the different components.
     	<Router>
 	    	<div id="body">
 	    	   <Row >
@@ -17,25 +18,26 @@ class App extends Component {
 
 	          				<CardTitle style={{fontSize:'3rem', padding:'20px'}}>Buy</CardTitle>
 	          				<CardText style={{ textAlign:'justify', textJustify:'inter-word', padding:'40px'}}>
-	          				Welcome to MECart, an online buying and selling platform exclusively for the students of MODEL ENGINEERING COLLEGE.
-MECians can buy used products like books,stationeries,mini drafter etc at lower rates from this site.
-							  </CardText>
-	           					<Link to="/buy"><Button id="buttonB" size="lg" block>Buy</Button></Link>
+												Welcome to MECart, an online buying and selling platform exclusively for the students of MODEL ENGINEERING COLLEGE.
+												MECians can buy used products like books,stationeries,mini drafter etc at lower rates from this site.
+										</CardText>
+	           				<Link to="/buy"><Button id="buttonB" size="lg" block>Buy</Button></Link>
 	        			</Card>
 	      			</Col>
 	      			<Col sm="6" style={{padding:'3%'}}>
-	        			<Card body inverse style={{backgroundColor:'rgba(0,0,0,0.5)', padding:'18%', height:'100%',width:'100%',border:'none'}}>
-	          				<CardTitle style={{fontSize:'3rem', padding:'20px'}}>Sell</CardTitle>
-	          				<CardText style={{ textAlign:'justify', textJustify:'inter-word', padding:'40px'}}>
-	          				MECart connects local people to buy, sell or exchange used goods and services by making it    fast and easy for anyone to post a listing on the web.MECians can sell used products like books,stationeries,mini drafters etc in this site.
-							  </CardText>
-	          					 <Link to="/sell"><Button id="buttonS" size="lg" block>Sell</Button></Link>
-	        			</Card>
+	        				<Card body inverse style={{backgroundColor:'rgba(0,0,0,0.5)', padding:'18%', height:'100%',width:'100%',border:'none'}}>
+										<CardTitle style={{fontSize:'3rem', padding:'20px'}}>Sell</CardTitle>
+										<CardText style={{ textAlign:'justify', textJustify:'inter-word', padding:'40px'}}>
+													MECart connects local people to buy, sell or exchange used goods and services by making it  
+													fast and easy for anyone to post a listing on the web.MECians can sell used products like books,stationeries,mini drafters etc in this site.
+										</CardText>
+										<Link to="/sell"><Button id="buttonS" size="lg" block>Sell</Button></Link>
+	        				</Card>
 	      			</Col>
-	    		</Row>
-	    	<Route path="/buy" component={BuyPage} />
-	    	<Route path="/sell" component={SellPage} />   
-	    	</div>
+	    		  </Row>
+						<Route path="/buy" component={BuyPage} />
+						<Route path="/sell" component={SellPage} />   
+	    		</div>
         </Router>
     );
   }
